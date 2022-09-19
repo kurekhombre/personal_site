704211bd86bede19a7998a32273b71a2d90d9b32
Karol <kurekdev@gmail.com>
2022-09-19 23:41:51 +0200

    new model project, changed static last projects into dynamic for loop + requirements.txt

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    subtitle = models.CharField(max_length=128, default="-")
+    description = models.CharField(max_length=300)
+    github_link = models.URLField(null=True, blank=True)
+    live_link = models.URLField(null=True, blank=True)
+    image = models.ImageField(upload_to='projects/')
+    added = models.DateTimeField(auto_created=True, auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html')
+    last_projects = Project.objects.all().order_by('-added')[:3]
+    return render(request, 'portfolio/index.html', {
+      'last_projects': last_projects
+    })
